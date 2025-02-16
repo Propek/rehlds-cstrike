c551c2f5cf60fb5c7adbd2c486103b0540bafdde
Hubert <hubercik93@gmail.com>
2025-02-16 21:28:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,58 +1,12 @@
-# Give everything time to initialize for preventing SteamCMD deadlock
-sleep 1
+#!/bin/bash
+cd /home/container
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
+# Output Current Java Version
+java -version ## only really needed to show what version is being used. Should be changed for different applications
 
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Switch to the container's working directory
-cd /home/container || exit 1
-
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-## just in case someone removed the defaults.
-if [ "${STEAM_USER}" == "" ]; then
-    echo -e "steam user is not set.\n"
-    echo -e "Using anonymous user.\n"
-    STEAM_USER=anonymous
-    STEAM_PASS=""
-    STEAM_AUTH=""
-else
-    echo -e "user set to ${STEAM_USER}"
-fi
-
-## if auto_update is not set or to 1 update
-#if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
-#    # Update Source Server
-#    if [ ! -z ${SRCDS_APPID} ]; then
-#        ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
-#    else
-#        echo -e "No appid set. Starting Server"
-#    fi
-#
-#else
-#    echo -e "Not updating game server as auto update was set to 0. Starting Server"
-#fi
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-HLDS_RUN_PATH=$(find -name "hlds_run" -print -quit)
-
-if [ -n "$HLDS_RUN_PATH" ]; then  # Check if the variable is not empty
-  printf "HLDS_RUN_PATH: $HLDS_RUN_PATH"
-  # ... use $HLDS_RUN_PATH ...
-else
-  printf "hlds_run not found!"
-  exit 1  # Or handle the error appropriately
-fi
-
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Run the Server
+${MODIFIED_STARTUP}
