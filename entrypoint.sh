09c62c990f24a50e49a6779458f60faa41667561
Hubert <hubercik93@gmail.com>
2025-02-16 15:24:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,20 +29,30 @@ else
 fi
 
 ## if auto_update is not set or to 1 update
-if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
-    # Update Source Server
-    if [ ! -z ${SRCDS_APPID} ]; then
-        ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
-    else
-        echo -e "No appid set. Starting Server"
-    fi
-
-else
-    echo -e "Not updating game server as auto update was set to 0. Starting Server"
-fi
+#if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
+#    # Update Source Server
+#    if [ ! -z ${SRCDS_APPID} ]; then
+#        ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+#    else
+#        echo -e "No appid set. Starting Server"
+#    fi
+#
+#else
+#    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+#fi
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
+HLDS_RUN_PATH=$(find -f -name "hlds_run" -print -quit)
+
+if [ -n "$HLDS_RUN_PATH" ]; then  # Check if the variable is not empty
+  printf "HLDS_RUN_PATH: $HLDS_RUN_PATH"
+  # ... use $HLDS_RUN_PATH ...
+else
+  printf "hlds_run not found!"
+  exit 1  # Or handle the error appropriately
+fi
+
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
